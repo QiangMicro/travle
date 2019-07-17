f932e580cdde9ab1e9dc439ade6a7edf40c27bb4
rose <rose_joh@163.com>
2019-07-17 13:44:53 +0800

    补充icon引入

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <HomeHeader></HomeHeader>
     <HomSwiper></HomSwiper>
+    <HomeIcon></HomeIcon>
   </div>
 
 </template>
@@ -9,11 +10,13 @@
 <script>
 import HomeHeader from './components/HomeHeader'
 import HomSwiper from './components/HomSwiper'
+import HomeIcon from './components/HomeIcon'
 export default {
   name:'Home',
   components:{
     HomeHeader,
-    HomSwiper
+    HomSwiper,
+    HomeIcon
   }
 }
 </script>
