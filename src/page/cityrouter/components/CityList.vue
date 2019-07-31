ff026b8020636e1447467c6cc705ee454805952e
rose <rose_joh@163.com>
2019-07-31 16:19:03 +0800

    betterscoll完成

--- src/page/cityrouter/components/CityList.vue
+++ src/page/cityrouter/components/CityList.vue
@@ -1,81 +1,88 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title  border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title  border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title  border-topbottom">热门城市</div>
-       <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">上海</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">三亚</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">香港</div>
-        </div>
-         <div class="button-wrapper">
-          <div class="button">杭州</div>
-        </div>
-         <div class="button-wrapper">
-          <div class="button">广州</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">成都</div>
+      <div class="area">
+        <div class="title  border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">上海</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">三亚</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">香港</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">广州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">成都</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title  border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
+      <div class="area">
+        <div class="title  border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title  border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">巴彦淖尔</div>
-        <div class="item border-bottom">巴彦淖尔</div>
-        <div class="item border-bottom">巴彦淖尔</div>
-        <div class="item border-bottom">巴彦淖尔</div>
-        <div class="item border-bottom">巴彦淖尔</div>
-        <div class="item border-bottom">巴彦淖尔</div>
-        <div class="item border-bottom">巴彦淖尔</div>
+      <div class="area">
+        <div class="title  border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">巴彦淖尔</div>
+          <div class="item border-bottom">巴彦淖尔</div>
+          <div class="item border-bottom">巴彦淖尔</div>
+          <div class="item border-bottom">巴彦淖尔</div>
+          <div class="item border-bottom">巴彦淖尔</div>
+          <div class="item border-bottom">巴彦淖尔</div>
+          <div class="item border-bottom">巴彦淖尔</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title  border-topbottom">C</div>
-      <div class="item-list">
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
-        <div class="item border-bottom">沧州</div>
+      <div class="area">
+        <div class="title  border-topbottom">C</div>
+        <div class="item-list">
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+          <div class="item border-bottom">沧州</div>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name:'CityList'
+  name:'CityList',
+  mounted (){
+    this.BScroll= new BScroll(this.$refs.wrapper)
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -96,7 +103,7 @@ export default {
     right 0
     bottom 0
     .title
-      line-height .44rem
+      line-height .55rem
       background #eeeeee
       padding-left .2rem
       color #666
