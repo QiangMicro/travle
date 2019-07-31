1557980ffd2a98f55dc2e669c508a0cb9cd1dc03
rose <rose_joh@163.com>
2019-07-31 16:32:28 +0800

    list样式完成

--- src/page/cityrouter/City.vue
+++ src/page/cityrouter/City.vue
@@ -3,18 +3,21 @@
     <CityHeader></CityHeader>
     <CitySwitch></CitySwitch>
     <CityList></CityList>
+    <List></List>
   </div>
 </template>
 <script>
 import CityHeader from '../cityrouter/components/CityHeader'
 import CitySwitch from '../cityrouter/components/CitySwitch'
 import CityList from '../cityrouter/components/CityList'
+import List from '../cityrouter/components/list'
 export default {
   name:'City',
   components:{
     CityHeader,
     CitySwitch,
-    CityList
+    CityList,
+    List
   }
 }
 </script>
