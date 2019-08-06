7a81c162b4124afd151e09d2c88df7f39ded6a2f
rose <rose_joh@163.com>
2019-08-06 11:39:15 +0800

    代码优化

--- src/page/cityrouter/components/list.vue
+++ src/page/cityrouter/components/list.vue
@@ -15,6 +15,7 @@
 <script>
 import { constants } from 'zlib';
 import { log } from 'util';
+import { clearTimeout, setTimeout } from 'timers';
 export default {
   name:'List',
   props:{
@@ -23,7 +24,8 @@ export default {
   },
   data(){
     return{
-      touchshow:false
+      touchshow:false,
+      timer:null
     }
   },
   // 利用计算属性定义名为letters的城市数组
@@ -44,17 +46,23 @@ export default {
     hadtouchstart(){
       this.touchshow=true
     },
+    // 通过函数截流的方法优化代码
     hadtouchmove(e){
       if(this.touchshow){
-        // 首个字母距离顶部的距离
-        const startY=this.$refs["A"][0].offsetTop
-        // 手指区域距离顶部的高度
-        const touchY=e.touches[0].clientY-79
-        // 判断手指所在的下标
-        const index=-( Math.floor((startY-touchY)/20))
-        if(index>=0 && index<this.letters.length){
-          this.$emit('change',this.letters[index])
+        if(this.timer){
+          clearTimeout(this.timer)
         }
+        this.timer=setTimeout(()=>{
+          // 首个字母距离顶部的距离
+          const startY=this.$refs["A"][0].offsetTop
+          // 手指区域距离顶部的高度
+          const touchY=e.touches[0].clientY-79
+          // 判断手指所在的下标
+          const index=-( Math.floor((startY-touchY)/20))
+          if(index>=0 && index<this.letters.length){
+            this.$emit('change',this.letters[index])
+          }
+        },16)
       }
     },
     hadtouchend(){
